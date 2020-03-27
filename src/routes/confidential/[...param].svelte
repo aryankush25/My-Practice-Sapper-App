16e08df226f2fccecd0c8be5a6dea50836076443
Aryan Agarwal <aryankush025@gmail.com>
2020-03-27 23:42:57 +0530

    custom routes added

--- src/routes/confidential/[...param].svelte
+++ src/routes/confidential/[...param].svelte
@@ -8,15 +8,34 @@
 	export let params;
 	export let query;
 	let queryKeysArray;
+	let title;
+
+	const getTitle = (param) => {
+		let temp = '';
+
+		for (let i = 0; i < param.length; i++) {
+			const element = param[i];
+			if (i === 0) {
+				temp = temp  + element
+			} else {
+				temp = temp + '_' + element
+			}
+		}
+
+		return temp
+	}
 
 	$: console.log('$$$$ params', params)
 	$: console.log('$$$$ query', query)
 
 	$: queryKeysArray = Object.keys(query)
+
+	$: title = getTitle(params.param)
+	
 </script>
 
 <svelte:head>
-	<title>{params}</title>
+	<title>{title}</title>
 </svelte:head>
 
 <div>This my Confidential Page </div>
